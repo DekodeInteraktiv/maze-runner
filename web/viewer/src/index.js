a2b0c732f8871cd0edd2a2e6fa14ef690aa91934
Peter Booker <mail@peterbooker.com>
2022-05-19 19:39:38 +0100

    Run from /viewer directory

--- web/viewer/src/index.js
+++ web/viewer/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/viewer">
     <Routes>
       <Route path="/" element={<App />}>
         <Route path=":game" element={<App />} />
