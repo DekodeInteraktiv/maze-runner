82bf0ad4b90a8363509b9abe4196eeb98871ef51
Peter Booker <mail@peterbooker.com>
2022-06-10 19:37:51 +0100

    Fix embedded apps

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -3,5 +3,5 @@ package assets
 import "embed"
 
 // content holds our static web server content.
-//go:embed static viewer controller
+//go:embed static viewer controller leaderboard register
 var Content embed.FS
